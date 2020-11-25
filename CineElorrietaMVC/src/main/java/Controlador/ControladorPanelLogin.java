fd092e4052ee5ae3d6f4070fca6d6d55b9422d84
evv23 <eva.setienfe@elorrieta.errekamari.com>
2020-11-25 16:25:27 +0100

    panelLogin y controlador para trbajar sobre el

---
+++ CineElorrietaMVC/src/main/java/Controlador/ControladorPanelLogin.java
@@ -0,0 +1,54 @@
+package Controlador;
+
+import Modelo.Modelo;
+import Vista.PanelLogin;
+import Vista.Vista;
+
+public class ControladorPanelLogin {
+	@SuppressWarnings("unused")
+	private Modelo modelo;
+	private Vista vista;
+	private Controlador controlador;
+	private PanelLogin panelLogin;
+	private final String usuario="usuario";
+	private final String contrasena="12345";
+	
+	
+	
+	
+	public ControladorPanelLogin(Modelo modelo, Vista vista, Controlador controlador) {
+		this.modelo = modelo;
+		this.vista = vista;
+		this.controlador = controlador;	
+	}
+	
+	public void mostrarPanelLogin() {
+		this.panelLogin = new PanelLogin(this);
+		this.vista.mostrarPanel(this.panelLogin);
+	}
+	
+	public void mostrarLoginError() {/*no se puede testear, es de la vista */
+		this.panelLogin.getLblError().setVisible(true);
+	}
+	public PanelLogin makePanelLogin(ControladorPanelLogin controladorLogin) {
+		return new PanelLogin(controladorLogin);
+	}
+
+	public void accionadoBotonEntrarPanelLogin(String usuario2, String pass) {
+		System.out.println("Ejecutando evento Boton Entrar");
+		System.out.println(usuario2);
+		System.out.println(pass);
+		
+		if(usuario2.equals(this.usuario) && pass.equals(this.contrasena)) {		
+			System.out.println("Usuario/Contraseña correctos");
+			this.controlador.navegarPanelGeneros();
+		}
+		else {
+			System.out.println("Usuario/contraseña incorrectos");
+			mostrarLoginError();
+		}
+		
+	}
+	
+	
+}
---
+++ CineElorrietaMVC/src/main/java/Vista/PanelLogin.java
@@ -0,0 +1,107 @@
+package Vista;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import Controlador.ControladorPanelLogin;
+
+public class PanelLogin extends JPanel {
+	private JTextField txtUsuario;
+	private JPasswordField passContrasena;
+	private JLabel lblError;
+	private JLabel lblLogin;
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelLogin(ControladorPanelLogin controladorPanelLogin) {
+		
+		setLayout(null);
+		
+		JLabel lblContrasena = new JLabel("Contrase\u00F1a:");
+		lblContrasena.setBounds(142, 233, 89, 14);
+		add(lblContrasena);
+		
+		JLabel lblUsuario = new JLabel("Usuario:");
+		lblUsuario.setBounds(142, 202, 70, 14);
+		add(lblUsuario);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(252, 199, 86, 20);
+		add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		passContrasena = new JPasswordField();
+		passContrasena.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				  if (e.getKeyCode()==KeyEvent.VK_ENTER){
+			    	   controladorPanelLogin.accionadoBotonEntrarPanelLogin(txtUsuario.getText()
+								, String.valueOf(getPassContrasena().getPassword()));
+			       }
+			}
+		});
+		passContrasena.setBounds(252, 230, 121, 20);
+		add(passContrasena);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				controladorPanelLogin.accionadoBotonEntrarPanelLogin(txtUsuario.getText()
+						, String.valueOf(getPassContrasena().getPassword()));
+				
+				
+			}
+		});
+		btnEntrar.setBounds(198, 327, 89, 23);
+		add(btnEntrar);
+		
+		lblError = new JLabel("usuario/contrase\u00F1a incorrectos");
+		lblError.setForeground(Color.RED);
+		lblError.setBounds(164, 283, 272, 14);
+		add(lblError);
+		
+		lblLogin = new JLabel("LOGIN");
+		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblLogin.setBounds(198, 112, 141, 55);
+		add(lblLogin);
+		lblError.setVisible(false);
+	
+
+	}
+
+	public JLabel getLblError() {
+		return lblError;
+	}
+
+	public void setLblError(JLabel lblError) {
+		this.lblError = lblError;
+	}
+
+	public JTextField getTxtUsuario() {
+		return txtUsuario;
+	}
+
+	public void setTxtUsuario(JTextField txtUsuario) {
+		this.txtUsuario = txtUsuario;
+	}
+
+	public JPasswordField getPassContrasena() {
+		return passContrasena;
+	}
+
+	public void setPassContrasena(JPasswordField passContrasena) {
+		this.passContrasena = passContrasena;
+	}
+
+}
