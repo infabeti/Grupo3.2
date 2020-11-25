1ff3b2f9b427e1c73cc849384b83905b18f69c10
evv23 <eva.setienfe@elorrieta.errekamari.com>
2020-11-25 16:30:28 +0100

    Controlador

--- CineElorrietaMVC/src/main/java/Controlador/Controlador.java
+++ CineElorrietaMVC/src/main/java/Controlador/Controlador.java
@@ -1,20 +1,34 @@
 package Controlador;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
 import Modelo.Modelo;
 import Vista.Vista;
 
 public class Controlador {
-
 	private Modelo modelo;
 	private Vista vista;
 	private ControladorPanelBienvenida controladorPanelBienvenida;
 	private ControladorPanelGeneros controladorPanelGeneros;
+	private ControladorPanelLogin controladorPanelLogin;
+	private ControladorPanelPeliculas controladorPanelPeliculas;
+	//private ControladorPanelResumen controladorPanelResumen;
+	//private ControladorPanelFin controladorPanelFin;
 	
 	public Controlador(Modelo modelo, Vista vista) {
 		this.modelo = modelo;
 		this.vista = vista;
 		this.controladorPanelBienvenida = new ControladorPanelBienvenida(this.modelo, this.vista, this);
 		this.controladorPanelGeneros = new ControladorPanelGeneros(this.modelo, this.vista, this);
+		this.controladorPanelLogin = new ControladorPanelLogin(this.modelo, this.vista, this);
+		this.controladorPanelPeliculas = new ControladorPanelPeliculas(this.modelo, this.vista, this);
+		//this.controladorPanelResumen = new ControladorPanelResumen(this.modelo, this.vista, this);
+		//this.controladorPanelFin = new ControladorPanelFin(this.modelo, this.vista, this);
 		this.navegarPanelBienvenida();
 	}
 	
@@ -22,9 +36,57 @@ public class Controlador {
 		System.out.println("Navegar panel Bienvenida");
 		this.controladorPanelBienvenida.mostrarPanelBienvenida();
 	}
+	public void navegarPanelLogin() {
+		System.out.println("Navegar panel Bienvenida");
+		this.controladorPanelLogin.mostrarPanelLogin();
+	}
+	
 	
 	public void navegarPanelGeneros() {
 		System.out.println("Navegar panel Generos");
 		this.controladorPanelGeneros.mostrarPanelGeneros();
+	}
+	
+	public void navegarPanelPeliculas(int genero) {
+		System.out.println("Navegar panel peliculas");
+		this.controladorPanelPeliculas.mostrarPanelPeliculas(genero);
+		
+	}
+	
+	/*public void navegarPanelResumen() {
+		System.out.println("Navegar panel Resumen");
+		this.controladorPanelResumen.mostrarPanelResumen();
+		
+	}
+	public void navegarPanelFin() {
+		System.out.println("Navegar panel Fin");
+		this.controladorPanelFin.mostrarPanelFin();
+		
+		
+	}*/
+	public ControladorPanelGeneros makeControladorGeneros(Modelo modelo, Vista vista, Controlador controlador) {
+		return new ControladorPanelGeneros(this.modelo, this.vista, this);
+	}
+	
+	//metodo que escribi en el fichero de logs los errores que le introduzcamos por parámetro
+	public static void escribirErrorEnLog(String error) {
+		
+		try {
+			FileWriter fr;
+			File fichero=new File("errores.log");
+			fr = new FileWriter(fichero,true);
+			BufferedWriter br= new BufferedWriter(fr);	//se crea el buffer para escibir
+			Date date = new Date(1);//revisar el 1, si no daba error
+			SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+			System.out.println(formatter.format(date));
+			br.write("["+formatter.format(date)+"]-> "+error); 	
+			br.newLine();//escribimos
+			br.close(); 		//cerramos el buffer
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Ha ocurrido un error al escribir en el fichero de errores");
+			e.printStackTrace();
+		}	 
 	}
 }
