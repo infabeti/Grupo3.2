d75b2cd1b56674de48d5c1236022679fd04beeea
AlexYepes1 <alexander.yepesma@elorrieta-errekamari.com>
2020-11-30 13:06:43 +0100

    ControladorPanelResumen y PanelResumen

--- CineElorrietaMVC/src/main/java/Controlador/ControladorPanelResumen.java
+++ CineElorrietaMVC/src/main/java/Controlador/ControladorPanelResumen.java
@@ -1,5 +1,81 @@
 package Controlador;
 
+import Modelo.Modelo;
+import Vista.PanelGeneros;
+import Vista.PanelLogin;
+import Vista.PanelPeliculas;
+import Vista.PanelResumen;
+import Vista.Vista;
+
 public class ControladorPanelResumen {
+	@SuppressWarnings("unused")
+	private Modelo modelo;
+	private Vista vista;
+	private Controlador controlador;
+	private PanelResumen panelResumen;
+	
+	public ControladorPanelResumen(Modelo modelo, Vista vista, Controlador controlador) {
+		this.modelo = modelo;
+		this.vista = vista;
+		this.controlador = controlador;	
+	}
+	//Muestra las peliculas del sabado y domingo con su duración
+	public void mostrarPanelResumen() {
+		this.panelResumen = new PanelResumen(this);
+		this.vista.mostrarPanel(this.panelResumen);
+		int horas,minutos;
+		
+		
+		//txtArea.append("Goodbye Cruel World\n");
+		
+		panelResumen.getTxtResumen().append("Sábado: \n");
+		
+		for(int i=0;i<ControladorPanelPeliculas.getPeliculasSabado().size();i++) {
+			
+			
+			
+			horas=(int) Math.floor((ControladorPanelPeliculas.getPeliculasSabado().get(i).getDuracion())/60);
+			minutos=(int) Math.round((ControladorPanelPeliculas.getPeliculasSabado().get(i).getDuracion())%60);
+			
+			panelResumen.getTxtResumen().append(ControladorPanelPeliculas.getPeliculasSabado().get(i).getTitulo()+"-> "+
+					horas+"h "+minutos+"m \n");
+			
+			
+		}
+		
+		panelResumen.getTxtResumen().append("\nDomingo: \n");
+		
+		for(int i=0;i<ControladorPanelPeliculas.getPeliculasDomingo().size();i++) {
+			horas=(int) Math.floor((ControladorPanelPeliculas.getPeliculasDomingo().get(i).getDuracion())/60);
+			minutos=(int) Math.round((ControladorPanelPeliculas.getPeliculasDomingo().get(i).getDuracion())%60);
+			
+			
+			panelResumen.getTxtResumen().append(ControladorPanelPeliculas.getPeliculasDomingo().get(i).getTitulo()+"-> "+
+					horas+"h "+minutos+"m \n");
+			
+			
+		}
+		
+	}
+	
+	public void accionadoBotonRechazarPanelResumen() {
+		controlador.navegarPanelGeneros();
+		ControladorPanelPeliculas.getPeliculasSabado().removeAll(ControladorPanelPeliculas.getPeliculasSabado());
+		ControladorPanelPeliculas.getPeliculasDomingo().removeAll(ControladorPanelPeliculas.getPeliculasDomingo());
+		
+	}
+	
+	public void accionadoBotonAceptarPanelResumen() {
+		controlador.navegarPanelFin();
+		
+		
+	}
+	
+	public PanelResumen makePanelResumen(ControladorPanelResumen controladorResumen) {
+		return new PanelResumen(controladorResumen);
+	}
+	
+	
+	
 
 }
--- CineElorrietaMVC/src/main/java/Vista/PanelResumen.java
+++ CineElorrietaMVC/src/main/java/Vista/PanelResumen.java
@@ -1,14 +1,59 @@
 package Vista;
 
+
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
+import Controlador.ControladorPanelLogin;
+import Controlador.ControladorPanelResumen;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PanelResumen extends JPanel {
+	private static JTextArea txtResumen;
 
 	/**
 	 * Create the panel.
 	 */
-	public PanelResumen() {
+	public PanelResumen(ControladorPanelResumen controladorPanelResumen) {
+		setLayout(null);
+		
+		JLabel lblResumen = new JLabel("RESUMEN:");
+		lblResumen.setBounds(91, 49, 96, 14);
+		add(lblResumen);
+		
+		txtResumen = new JTextArea();
+		txtResumen.setBounds(91, 96, 302, 203);
+		txtResumen.setEnabled(false);
+		add(txtResumen);
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		btnAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				controladorPanelResumen.accionadoBotonAceptarPanelResumen();
+			}
+		});
+		btnAceptar.setBounds(91, 353, 89, 23);
+		add(btnAceptar);
+		
+		JButton btnRechazar = new JButton("Rechazar");
+		btnRechazar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				controladorPanelResumen.accionadoBotonRechazarPanelResumen();
+			}
+		});
+		btnRechazar.setBounds(304, 353, 89, 23);
+		add(btnRechazar);
 
 	}
 
+	public static JTextArea getTxtResumen() {
+		return txtResumen;
+	}
+
+
+	
 }
